6956d5e7be1b0cc598e92a413e16097f09e609d7
Alex <gw@Gunkow-Large-Pro.local>
2018-05-24 12:23:53 +0300

    conf

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    ID = "c5541cc4fd834258b103"
+    ID = os.environ.get("CLIENT_ID")
 
-    SECRET = "51c14ecefcaf43f86f360e8bd96f902e2b3eb4cf"
+    SECRET = os.environ.get("SECRET")
 
